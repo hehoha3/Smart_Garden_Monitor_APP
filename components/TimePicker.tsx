5c1b4fad4d654ea014c81e6f4d55a0270f056b47
hehoha3 <duyanhvipprox9@gmail.com>
2024-12-03 09:37:18 +0700

    config off time picker

--- components/TimePicker.tsx
+++ components/TimePicker.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import DateTimePicker, { DateTimePickerEvent } from "@react-native-community/datetimepicker"
 
@@ -61,12 +61,44 @@ export default function TimePicker({ title, initialTimeOn, initialTimeOff, onTim
     //! Set Time_OFF
     const onChangeTimeOff = (e: DateTimePickerEvent, selectedTime?: Date) => {
         if (e.type === 'set' && selectedTime) {
-            setTimeOff(selectedTime);
-            setFormattedTimeOff(formatTime(selectedTime));
+            // Check the difference of time on and time off
+            const newOffTime = selectedTime;
+            const timeDifference = (newOffTime.getTime() - timeOn.getTime()) / (1000 * 60 * 60);
 
-            onTimeSet(formattedTimeOn, formatTime(selectedTime));  // Send time data to Firebase
+            if (timeDifference > 2 || timeDifference <= 0) {
+                Alert.alert(
+                    '!!! Cảnh báo !!!',
+                    'Bạn đã đặt thời gian sử dụng quá lâu cho thiết bị, Hãy đặt lại nó !',
+                    [{
+                        text: 'OK', onPress: () => {
+                            setShowSetTimeOff(false);
+                            setTimeout(() => {
+                                setShowSetTimeOff(true);
+                            }, 100);
+                        }
+                    }]
+                )
+            } else {
+                setTimeOff(selectedTime);
+                setFormattedTimeOff(formatTime(selectedTime));
+
+                onTimeSet(formattedTimeOn, formatTime(selectedTime));  // Send time data to Firebase
+                setShowSetTimeOff(false);
+            }
+        } else if (e.type === 'dismissed') {
+            Alert.alert(
+                '!!! Cảnh báo !!!',
+                'Bạn cần phải đặt thời gian tắt cho thiết bị !',
+                [{
+                    text: 'OK', onPress: () => {
+                        setShowSetTimeOff(false);
+                        setTimeout(() => {
+                            setShowSetTimeOff(true);
+                        }, 100);
+                    }
+                }]
+            )
         }
-        setShowSetTimeOff(false);
     }
 
     //! Clear both times
@@ -79,17 +111,18 @@ export default function TimePicker({ title, initialTimeOn, initialTimeOff, onTim
 
     //! -------------------------------------- RETURN --------------------------------------
     return (
-        <View className='flex-row justify-start items-center mb-5'>
+        <View className='flex-row justify-start items-center min-h-[35px] mb-5'>
             {/* TITLE */}
-            <Text className='min-w-[107px] text-white font-bold text-xl mr-4'>{title} timer:</Text>
+            <Text className='min-w-[105px] text-white font-bold text-xl'>{title} timer</Text>
+            <Text className='text-white font-bold text-xl mr-2'>:</Text>
 
 
             {/* ON TIME */}
-            <View className='flex-row justify-start items-center mr-3'>
+            <View className='flex-row justify-start items-center mr-1'>
                 <Text className='text-white'>On Time: </Text>
                 {formattedTimeOn ?
-                    <Text className='text-white'>{formattedTimeOn}</Text> :
-                    <TouchableOpacity activeOpacity={0.7} className='bg-red-700 p-1 rounded-lg justify-center items-center' onPress={showModeOn}>
+                    <Text className='text-white'>{formattedTimeOn} |</Text> :
+                    <TouchableOpacity activeOpacity={0.7} className='bg-blue-700 p-1 rounded-lg justify-center items-center' onPress={showModeOn}>
                         <Text className='text-white'>Not Set</Text>
                     </TouchableOpacity>
                 }
@@ -100,11 +133,11 @@ export default function TimePicker({ title, initialTimeOn, initialTimeOff, onTim
 
 
             {/* OFF TIME */}
-            <View className='flex-row justify-start items-center mr-3'>
+            <View className='flex-row justify-start items-center mr-2'>
                 <Text className='text-white'>Off Time: </Text>
                 {formattedTimeOff ?
                     <Text className='text-white'>{formattedTimeOff}</Text> :
-                    <TouchableOpacity activeOpacity={0.7} className='bg-red-700 p-1 rounded-lg justify-center items-center' onPress={showModeOn}>
+                    <TouchableOpacity activeOpacity={0.7} className='bg-blue-700 p-1 rounded-lg justify-center items-center' onPress={showModeOn}>
                         <Text className='text-white'>Not Set</Text>
                     </TouchableOpacity>
                 }
